25d402d2b6784d86231a74f4e61cda174547fa1e
viktorsht <viktorshantus@gmail.com>
2023-02-15 12:23:50 -0300

    time active

--- App/Models/TimeActive.php
+++ App/Models/TimeActive.php
@@ -8,7 +8,7 @@ class TimeActive{
     public function getTimeActive($employee, $city, $day){
         $conn = Connection::getDb();
         $query = '
-        SELECT ta.id, t.time, ta.fk_employee AS employee,ta.fk_city AS city, ta.fk_day AS day  FROM time_active AS ta
+        SELECT ta.id, t.time, ta.fk_employee AS employee,ta.fk_city AS city, ta.fk_day AS day FROM time_active AS ta
         INNER JOIN time AS t ON (ta.fk_time = t.id)
         WHERE ta.fk_employee = :fk_employee AND ta.fk_city = :fk_city AND ta.fk_day = :fk_day
         ';
